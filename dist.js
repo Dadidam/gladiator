1ead790beb471a47cdaf14db2e25d80409a6eca1
Ilya Vorontsov <vorontsov@moedelo.org>
2016-07-22 17:45:47 +0300

    Add package.json and common dependencies

---
+++ dist.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const App = () => {
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "div",
+            { className: "nice" },
+            React.createElement("span", null)
+        )
+    );
+};
---
+++ src.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const App = () => {
+    return (
+        <div>
+            <div className="nice"><span></span></div>
+        </div>
+    )
+}
