e3c05bbf60e833f646562535b20572718dfddbbb
Ilya Vorontsov <vorontsov@moedelo.org>
2017-03-31 20:09:47 +0300

    Add antd support && less support

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'react', 'stage-2']
                 }
+            },
+            {
+                test: /\.css$/,
+                loader:'style!css'
+            },
+            {
+                test: /\.less$/,
+                loader:'style-loader!css-loader!less-loader'
             }
         ]
     },
